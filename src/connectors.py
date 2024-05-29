5669dda87f341d684656f4a9b5fea3fa04ae8a1f
MatthewCorney <Matthew_corney@yahoo.co.uk>
2024-05-29 20:04:38 +0100

    feat: added memegraph support

--- src/connectors.py
+++ src/connectors.py
@@ -1,6 +1,7 @@
 from neo4j import GraphDatabase
 import json
 from typing import Optional
+from gqlalchemy import Memgraph
 
 
 class Client:
@@ -39,6 +40,37 @@ class Neo4jClient(Client):
         with self.driver.session() as session:
             result = session.run(query, parameters)
             return [record.data() for record in result]
+
+    def query_as_json(self, cypher_query: str, parameters=None):
+        result = self.query(cypher_query, parameters)
+        self.close()
+        return result
+
+
+class MemGraph(Client):
+    def __init__(self, address, port, username, password):
+        super().__init__()
+        self.driver = Memgraph(host=address,
+                               port=port,
+                               username=username,
+                               password=password)
+
+    def close(self):
+        """
+        Closes the connection
+        :return:
+        """
+        raise NotImplementedError
+
+    def query(self, query: str, parameters: Optional[dict] = None):
+        """
+        Buils a session
+        :param cypher_query:
+        :param parameters:
+        :return:
+        """
+        result = self.driver.execute_and_fetch(query, parameters=parameters)
+        return [r for r in result]
 
     def query_as_json(self, cypher_query: str, parameters=None):
         result = self.query(cypher_query, parameters)
